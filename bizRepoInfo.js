698b1efa642753f847fa23afeaf43fed012c040f
kross <kross31@krosss-MacBook-Pro.local>
2020-02-24 14:23:12 +0900

    headless fixed

--- bizRepoInfo.js
+++ bizRepoInfo.js
@@ -4,11 +4,7 @@ const capirStatus=async(code,year,repocode)=>{
   //증자,감자 현황
   try {
     const browser=await puppeteer.launch({
-<<<<<<< HEAD
       headless: true,
-=======
-      headless: tru,
->>>>>>> b243339261ff7fd6d8e2cb656cb24c5ad7f02cbe
       args:['--no-sandbox']
     })
 
